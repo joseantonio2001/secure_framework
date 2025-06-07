83769d43511245c43eb11cb888d3293288e2622c
José Antonio <jatorrescoca@gmail.com>
2025-06-07 14:30:09 +0200

    feat: Initialise secure_framework gem structure

---
+++ lib/secure_framework.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "secure_framework/version"
+
+module SecureFramework
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/secure_framework/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SecureFramework
+  VERSION = "0.1.0"
+end
